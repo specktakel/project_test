a364c541178af8c58cb9a88a30b6736feb7efadb
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2026-04-09 09:29:53 +0200

    string formatting

--- project_test/main.py
+++ project_test/main.py
@@ -3,6 +3,18 @@ import scipy
 
 
 def square(x: int) -> int:
-    """Returns the square of an int"""
+    """Returns the square of an int
+    
+    Parameters
+    ----------
+        x : int
+            Integer to be squared
+
+    Returns
+    -------
+        x**2 : int
+            Square of an integer x
+    """
+
     return x**2
 
